81d3c2529d6c9fddf3a3a4c10c89125caa2e328a
Dimitri Maia <dimitrimaia39@gmail.com>
2025-06-05 22:39:36 -0300

    22:39

--- testes.py
+++ testes.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import re
 
 # Carregar chave da API via variável de ambiente
-GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY_ENV') 
+GEMINI_API_KEY = 'AIzaSyDTN2wVwLueQnJkF_8WnUsNDNagw0m8keM'
 
 if not GEMINI_API_KEY:
     raise ValueError("Erro: variável de ambiente GEMINI_API_KEY não está definida")
