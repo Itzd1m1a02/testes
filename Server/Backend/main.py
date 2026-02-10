b51a4b69ebe3f0e5971101f33a470fff78014e08
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2026-02-09 22:48:40 -0300

    agora busca dentro de um banco de dados
    so add um banco de dados e ajustei para ele consultar nele

--- Server/Backend/main.py
+++ Server/Backend/main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
 import os
+
+from database import maq, LocalConecao, UsuarioDB, Base
 
 app = FastAPI()
 
 basedir= os.path.abspath(os.path.dirname(__file__))
 frontdir= os.path.join(basedir, '../Frontend') 
+
+#Se nao tive, cria
+Base.metadata.create_all(bind=maq)
+
+def get_db():
+    db = LocalConecao()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # PERMISSÃO (CORS): Necessário para o seu HTML falar com o Python
 app.add_middleware(
@@ -18,7 +31,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Definimos a estrutura que o JSON deve ter
 class LoginSchema(BaseModel):
     email: str
     senha: str
@@ -32,16 +44,13 @@ async def read_root():
 
 
 @app.post("/login")
-def realizar_login(dados: LoginSchema):
-    # O JSON chega aqui já convertido em objeto 'dados'
-    print(dados) # Imprime o objeto completo
-    print(f"E-mail recebido: {dados.email}")
-    
-    # Simulação de verificação
-    if dados.email == "joaozinho@email.com" and dados.senha == "123":
-        return {"status": 200, "mensagem": "Login efetuado com sucesso!"}
-    
-    return {"status": 401, "mensagem": "Usuário ou senha incorretos."}
+def realizar_login(dados: LoginSchema, db: Session = Depends(get_db)):
+    usuario = db.query(UsuarioDB).filter(UsuarioDB.email == dados.email).first()
+    if not usuario:
+        return {"status": 404, "mensagem": "Quem cargas d'água é esse? Cadastre-se primeiro!"}
+    if usuario.senha != dados.senha:
+        return {"status": 401, "mensagem": "Desconfio mas nao posso provar... A senha esta errada"}
+    return {"status": 200, "mensagem": "Ola, bem-vindo de volta"}
 
 class CadastroSchema(BaseModel):
     email: str
@@ -50,7 +59,20 @@ class CadastroSchema(BaseModel):
     nascimento: str
 
 @app.post("/cadastro")
-def processar_cadastro(dados: CadastroSchema):
-    print(f"Novo cadastro: {dados.email}, CPF: {dados.cpf}")
-    return {"status": 200, "mensagem": "Cadastro realizado com sucesso!"}
+def processar_cadastro(dados: CadastroSchema, db: Session = Depends(get_db)):
+    usuariojatem = db.query(UsuarioDB).filter(UsuarioDB.email == dados.email).first()
+    if usuariojatem:
+        return {"status": 400, "mensagem": "Eita, esse email ja ta em uso. Tente outro!"}
+    
+    novo_ze_ruela = UsuarioDB(
+        email=dados.email,
+        senha=dados.senha, #salva sha depois
+        cpf=dados.cpf,
+        nascimento=dados.nascimento
+    )
+    db.add(novo_ze_ruela)
+    db.commit()
+    db.refresh(novo_ze_ruela)
+    print(f"Novo usuário add: {novo_ze_ruela.email}")
+    return {"status": 201, "mensagem": "Cadastro realizado com sucesso! Agora é só fazer login, vai la ser feliz"}
 
