cc5fbf2014b383ff5752c26602e3aed39b030824
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2026-02-10 20:04:57 -0300

    ok

--- Server/Backend/main.py
+++ Server/Backend/main.py
@@ -57,7 +57,7 @@ class LoginSchema(BaseModel):
 
 app.mount("/static", StaticFiles(directory=frontdir), name="static") 
 
-@app.get("/L")
+@app.get("/")
 async def read_root():
     return FileResponse(os.path.join(frontdir, "Login.html")) 
 
