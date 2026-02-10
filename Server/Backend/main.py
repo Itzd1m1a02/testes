5b8fed1a34e7939ea7da70fae488a953d121c13c
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2026-02-09 21:55:52 -0300

    Silas

--- Server/Backend/main.py
+++ Server/Backend/main.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
+
+basedir= os.path.abspath(os.path.dirname(__file__))
+frontdir= os.path.join(basedir, '../Frontend') 
 
 # PERMISSÃO (CORS): Necessário para o seu HTML falar com o Python
 app.add_middleware(
@@ -19,15 +23,16 @@ class LoginSchema(BaseModel):
     email: str
     senha: str
 
-@app.mount("/static", StaticFiles("../Frontend"), name="static")
+app.mount("/static", StaticFiles(directory=frontdir), name="static") 
 
 @app.get("/")
 async def read_root():
-    return FileResponse("../Frontend/Login.html") # Retorna o arquivo HTML para o navegador
+    return FileResponse(os.path.join(frontdir, "Login.html")) 
+
 
 
 @app.post("/login")
-async def realizar_login(dados: LoginSchema):
+def realizar_login(dados: LoginSchema):
     # O JSON chega aqui já convertido em objeto 'dados'
     print(dados) # Imprime o objeto completo
     print(f"E-mail recebido: {dados.email}")
@@ -37,3 +42,15 @@ async def realizar_login(dados: LoginSchema):
         return {"status": 200, "mensagem": "Login efetuado com sucesso!"}
     
     return {"status": 401, "mensagem": "Usuário ou senha incorretos."}
+
+class CadastroSchema(BaseModel):
+    email: str
+    senha: str
+    cpf: str
+    nascimento: str
+
+@app.post("/cadastro")
+def processar_cadastro(dados: CadastroSchema):
+    print(f"Novo cadastro: {dados.email}, CPF: {dados.cpf}")
+    return {"status": 200, "mensagem": "Cadastro realizado com sucesso!"}
+
